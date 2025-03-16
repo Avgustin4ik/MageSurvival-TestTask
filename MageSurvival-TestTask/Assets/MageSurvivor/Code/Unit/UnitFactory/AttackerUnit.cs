5f37125f7976a2a60bec1370c60471c128185398
Avgustin4ik <avgustin4ik68@gmail.com>
2025-03-16 22:34:26 +0700

    attack cooldown

--- MageSurvival-TestTask/Assets/MageSurvivor/Code/Unit/UnitFactory/AttackerUnit.cs
+++ MageSurvival-TestTask/Assets/MageSurvivor/Code/Unit/UnitFactory/AttackerUnit.cs
@@ -8,14 +8,22 @@
     public class AttackerUnit : CharacterUnitBase, IAttacker
     {
         private IDisposable _stream;
+        private bool _isAttacking;
         public IDamageable _target { get; set; }
         public float AttackRange { get; set; }
         public float Damage { get; private set; }
-
+        private IDisposable _attackCooldown;
         public void TryAttack(IDamageable target)
         {
+            if(_isAttacking) return;
+            _isAttacking = true;
             Debug.Log("AttackerUnit TryAttack");
             target.TakeDamage(Damage);
+            
+            _attackCooldown.Dispose();
+            _attackCooldown = Observable.Timer(TimeSpan.FromSeconds(1f/Config.AttackSpeed))
+                .Subscribe(_ => _isAttacking = false)
+                .AddTo(this.Disposables);
         }
 
         public void SetTarget(IDamageable target)
